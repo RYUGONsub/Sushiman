5e163160a32ffa892ab2262f27f3833ba5242e87
BushoRyu <ryugon.s.0829@gmail.com>
2022-02-12 00:34:24 +0900

    1

---
+++ js/footer.js
@@ -0,0 +1,4 @@
+javascript:(function()
+{
+    document.write('<div class="footer"><div class="footer-logo">すしまんのリンク</div><div class="footer-list"><ul><li><a href="https://twitter.com/MUTOSHONNN">Twitter</a></li><li><a href="https://www.youtube.com/channel/UC-m7p-0AXNNxwN6LYXMOFeA">YouTubeChannel</a></li></ul></div></div>');
+})()
---
+++ js/header.js
@@ -0,0 +1,4 @@
+javascript:(function()
+{
+    document.write('<div class="header"><div class="header-logo"><a href="index.html">すし☆すた</a></div><div class="header-list"><ul><li><a href="Video.html">動画一覧</a></li><li><a href="info-blog.html">ブログ館</a></li><li><a href="prof.html">すしすたとは•••</a></li><li><a href="infoTab.html">お知らせ</a></li></ul></div></div>');
+})()
